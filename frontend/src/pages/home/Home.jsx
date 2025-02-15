a903b50bc00501a5499afa79f7e994999a3d576e
Fabiana Pereira <fabianagenila@gmail.com>
2025-02-15 19:38:47 -0300

    Recommended Books section setup

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "./Banner";
 import TopSellers from "./TopSellers";
+import Recommended from './Recommended';
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <TopSellers />
+      <Recommended />
     </div>
   );
 };
